55cfaaa3072e2686e96e5fcaad6bb0cf5adfb89a
Maxim Mitin <maximus405@gmail.com>
2021-12-04 22:41:46 +0300

    Initial Commit

---
+++ CompositionalLayout_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayout_test
+//
+//  Created by Maxim Mitin on 4.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
